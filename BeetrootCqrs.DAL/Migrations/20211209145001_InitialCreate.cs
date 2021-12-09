27dab42ce5f1d02ac3eb72ec4d4c2ab31a91ad59
Taras Karpuk <taras.karpuk@gmail.com>
2021-12-09 16:53:19 +0200

    Add DB and migration

---
+++ BeetrootCqrs.DAL/Migrations/20211209145001_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeetrootCqrs.DAL.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    IpAddress = table.Column<string>(type: "TEXT", maxLength: 16, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Text = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    AddressId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_AddressId",
+                table: "Messages",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_Date",
+                table: "Messages",
+                column: "Date");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+        }
+    }
+}
